28e9582b2611058f4a288f9765ea826fc85e9c8f
hkl-iii <rahleggon@gmail.com>
2021-05-15 22:00:26 -0500

    "added line 6 in music/urlspy. Now "Get_Song_By_Id request should work in Django REST API(Postman)"

--- music_library_project/music/urls.py
+++ music_library_project/music/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('music/', views.Songlist.as_view())
+    path('music/', views.Songlist.as_view()),
+    path('music/<int:pk>/', views.SongDetail.as_view()),
 ]
